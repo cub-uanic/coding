da2232f6b23d09cc8be7f5384f0ce95eb199e9c0
Oleg Kostyuk <cub.uanic@gmail.com>
2020-09-20 23:25:31 +0300

    Re-worked codingame solution template according to hint/lint (see fairNumbering)

--- src/main/cub/codingame/puzzle/solution-template.js
+++ src/main/cub/codingame/puzzle/solution-template.js
@@ -1,46 +1,48 @@
-'use strict';
-
 process.stdin.resume()
 process.stdin.setEncoding('utf-8')
 
 let ____inputString = ''
 let ____currentLine = 0
-let ____running_locally = 1
+const ____runningLocally = 1
 
-process.stdin.on('data', inputStdin => {
-    ____inputString += inputStdin
-});
+process.stdin.on('data', (inputStdin) => {
+  ____inputString += inputStdin
+})
 
-process.stdin.on('end', _ => {
-    ____inputString = ____inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''))
+process.stdin.on('end', (_) => {
+  ____inputString = ____inputString
+    .replace(/\s*$/, '')
+    .split('\n')
+    .map((str) => str.replace(/\s*$/, ''))
 
-    main(1)
-});
+  main(1)
+})
 
 function readline() {
-    return ____inputString[____currentLine++]
+  // eslint-disable-next-line no-plusplus
+  return ____inputString[____currentLine++]
 }
 
-
-/**
+/*
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
- **/
+ */
 main()
 
 function main() {
-    // trick for debug locally
-    if (typeof ____running_locally !== "undefined" && arguments.length === 0) {
-        return
-    }
+  // trick for debug locally
+  if (typeof ____runningLocally !== 'undefined' && arguments.length === 0) {
+    return
+  }
 
-    // your program below
-    // no globals should be used
-    let inputs
-    inputs = readline().split(' ')
-    const w = parseInt(inputs[0])
-    const h = parseInt(inputs[1])
-    // .... and so on
+  // your program below
+  // no globals should be used
+  let inputs
+  // eslint-disable-next-line prefer-const
+  inputs = readline().split(' ')
+  // eslint-disable-next-line no-unused-vars
+  const w = parseInt(inputs[0], 10)
+  // eslint-disable-next-line no-unused-vars
+  const h = parseInt(inputs[1], 10)
+  // .... and so on
 }
