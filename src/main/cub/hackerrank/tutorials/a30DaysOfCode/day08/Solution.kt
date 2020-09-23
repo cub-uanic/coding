df3de52852340d291299de8fccf5c88f2467cadb
Oleg Kostyuk <cub.uanic@gmail.com>
2020-09-23 04:51:45 +0300

    Added solution for hackerrank/tutorials/30-days-of-code/day08

---
+++ src/main/cub/hackerrank/tutorials/a30DaysOfCode/day08/Solution.kt
@@ -0,0 +1,22 @@
+package cub.hackerrank.tutorials.a30DaysOfCode.day08
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    val pb: MutableMap<String, Int> = HashMap()
+    for (i in 0 until n) {
+        val name = scanner.next()
+        val phone = scanner.nextInt()
+        pb[name] = phone
+    }
+    while (scanner.hasNext()) {
+        val s = scanner.next()
+        println(
+            if (pb.containsKey(s)) s + "=" + pb[s].toString() else "Not found"
+        )
+    }
+    scanner.close()
+}
