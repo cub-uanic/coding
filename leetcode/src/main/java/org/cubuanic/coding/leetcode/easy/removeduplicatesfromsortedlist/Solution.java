aeb233a26ba273ef4c9c6509db6968ee73044fdf
Oleg Kostyuk <cub.uanic@gmail.com>
2021-12-03 06:52:27 +0200

    solved remove-linked-list-elements

---
+++ leetcode/src/main/java/org/cubuanic/coding/leetcode/easy/removeduplicatesfromsortedlist/Solution.java
@@ -0,0 +1,12 @@
+package org.cubuanic.coding.leetcode.easy.removeduplicatesfromsortedlist;
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) curr.next = curr.next.next;
+            else curr = curr.next;
+        }
+        return head;
+    }
+}
