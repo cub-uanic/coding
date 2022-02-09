141367e373d433829148f3e532d500d2e058751c
Oleg Kostyuk <cub.uanic@gmail.com>
2022-02-09 08:14:40 +0200

    added Groovy4 support, downgrade to Scala2 (buggy IDEA)

---
+++ leetcode/src/main/groovy/org/cubuanic/coding/testapp/GroovyTestApp.groovy
@@ -0,0 +1,7 @@
+package org.cubuanic.coding.testapp
+
+class GroovyTestApp {
+  static void main(String[] args) {
+    println 'Hello from Groovy!'
+  }
+}
---
+++ leetcode/src/main/scala/org/cubuanic/coding/testapp/ScalaTestApp.scala
@@ -0,0 +1,7 @@
+package org.cubuanic.coding.testapp
+
+object ScalaTestApp {
+  def main(args: Array[String]): Unit = {
+    println("Hello from Scala!")
+  }
+}
